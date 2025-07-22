e5299a8d893cf74c93fbcdbc970b8e3f25eec021
Jicotemx <o.davalos.o@gmail.com>
2025-07-22 16:17:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -241,5 +241,4 @@ def importar_base():
         return f"Error al importar: {str(e)}", 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run()
